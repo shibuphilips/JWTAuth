de04aa63e56380f633fc7b15efa4dc6118a5cfd2
shibuphilips <shibu.phils@gmail.com>
2024-10-13 01:32:35 -0500

    Initial commit

---
+++ API/Dtos/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Dtos
+{
+    public class AuthResponseDto
+    {
+        public string? Token { get; set; } = string.Empty;
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+    }
+}
